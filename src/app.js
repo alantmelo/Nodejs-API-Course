beb338906408c2e22305fe89cb941c1a38f8b37b
Alan Tavares de Melo <alantmelo@gmail.com>
2019-01-02 12:39:09 -0300

    CORS

--- src/app.js
+++ src/app.js
@@ -11,10 +11,19 @@ mongoose.connect(config.connectionString, {
 
 mongoose.set('useCreateIndex', true);
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({
+    limit: '5mb'
+}));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
+    next();
+});
 
 // Load Models
 const Product = require('./models/product');
